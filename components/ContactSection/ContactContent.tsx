bde30b0cb9316ca9c570d32656fef2a8f535ea98
Hector Silva-Robles <hectorsilvarobles@Hectors-MacBook-Air.local>
2024-08-21 22:30:06 -0700

    starting

--- components/ContactSection/ContactContent.tsx
+++ components/ContactSection/ContactContent.tsx
@@ -4,7 +4,7 @@ const ContactContent: React.FC = () => {
   return (
     <>
       <h2 className="mt-5 text-6xl font-semibold leading-[83px] text-neutral-600 max-md:max-w-full max-md:text-4xl max-md:leading-[57px]">
-        Contact The Kombong and Let's Build Together
+        Contact The Kombong and Lets Build Together
       </h2>
       <p className="mt-5 text-lg leading-6 text-gray-400 max-md:max-w-full">
         Sed turpis blandit fringilla est semper. Sed sodales nisl ullamcorper arcu egestas at tortor ante
