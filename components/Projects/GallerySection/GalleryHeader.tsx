b97cff7dd56230e5907a8114de5dc1354ea33238
Hector Silva-Robles <hectorsilvarobles@Hectors-MacBook-Air.local>
2024-10-07 07:41:49 -0700

    fix build error

--- components/Projects/GallerySection/GalleryHeader.tsx
+++ components/Projects/GallerySection/GalleryHeader.tsx
@@ -7,13 +7,12 @@ const GalleryHeader: React.FC = () => {
         Gallery
       </h2>
       <h1 className="mt-4 text-5xl font-semibold leading-tight text-center max-md:max-w-full max-md:text-4xl">
-        Transformations We've Completed
+        {"Transformations We've Completed"}
       </h1>
       <p className="mt-4 text-xl font-medium leading-7 text-center max-md:max-w-full">
-        Explore the stunning transformations we've made for clients. Whether
-        it's a bold exterior refresh or an intricate interior job, these
-        projects demonstrate the skill and passion we bring to every space we
-        work on.
+        {
+          "Explore the stunning transformations we've made for clients. Whether it's a bold exterior refresh or an intricate interior job, these projects demonstrate the skill and passion we bring to every space we work on."
+        }
       </p>
     </header>
   );
